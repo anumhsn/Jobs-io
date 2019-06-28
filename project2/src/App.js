c14ed12a7dc0832f337740141c2dd671816120cd
anumhsn22 <anumh123@gmail.com>
2019-06-28 14:42:14 -0400

    added input

--- project2/src/App.js
+++ project2/src/App.js
@@ -30,7 +30,7 @@ class App extends React.Component{
           <Header />
         </header>
         <main>
-          <Search jobs={this.state.jobs}/>
+          <Search />
         </main>
         
       </div>
--- project2/src/components/Search.jsx
+++ project2/src/components/Search.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 
 class Search extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            location: ''
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+    
     render(){
         return(
             <div className="search">
                 <form>
-                    <input name="location" placeholder="location..."/>
+                    <input name="location" placeholder="location..." type="text" onChange={this.handleChange}/>
                     
                 </form>
             </div>
